6c6f8637ad9b59065aa844afa76b998656942c22
Lizbeth Ricaño Bautista <lizbetricano18@gmail.com>
2020-06-19 15:24:41 -0500

    correcciónes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Validacion';
+  show:boolean=false;
+  buttonName:any='Mostrar componente';
+
+  mostrar(){
+    this.show = !this.show;
+    if(this.show){  
+      this.buttonName = "Ocultar componente";
+    }else{
+      this.buttonName = "Mostrar componente";
+    }
+  }
 }
